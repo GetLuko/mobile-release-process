94a2e7dd26139f5bb08fb6638a84946604391202
ludwig <hello@ludwigvantours.dev>
2022-09-21 22:40:28 +0200

    lodash

--- src/release/utils/isSemVer.ts
+++ src/release/utils/isSemVer.ts
@@ -1,10 +1,9 @@
-import { overEvery } from "lodash";
+import overEvery from "lodash.overevery";
 
 import { AnswerReleaseNumber } from "../patch/prepareAlphaBranch";
 
 export const isSemVer = overEvery<AnswerReleaseNumber["releaseNumber"]>([
   (releaseNumber) => typeof releaseNumber === "string",
   (releaseNumber) => releaseNumber.split(".").length === 3,
-  (releaseNumber) =>
-    releaseNumber.split(".").every((number) => number.match(/^\d+$/)),
+  (releaseNumber) => releaseNumber.split(".").every((number) => number.match(/^\d+$/)),
 ]);
